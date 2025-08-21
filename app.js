ede72a13701d37f9ff9ee64816c70c0fdb5fbd6d
Zin Lin Htet <zinlinhtet.devops@gmail.com>
2025-08-21 13:16:35 +0700

    Add: CORS in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const todosRouter = require('./routes/todos');
 const healthRouter = require('./routes/health');
 
